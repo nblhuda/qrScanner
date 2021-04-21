a6e130668ab472aa0c168d88d56dd6d083f1ba3f
User <user@Users-MacBook-Pro.local>
2021-04-21 23:00:32 +0800

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { ThrowStmt } from '@angular/compiler';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,51 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  scanActive = true;
+  scanResult = null;
+  @ViewChild('video', {static : false}) video : ElementRef;
 
+  videoElement : any;
+
+  constructor(private toastCtrl: ToastController) {}
+
+  ngAfterViewInit() {
+    // this.canvasElement = this.canvas.nativeElement;
+    // this.canvasContext = this.canvasElement.getContext('2d');
+    this.videoElement = this.video.nativeElement;
+  }
+
+  async startScan() {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: {facingMode: 'environment'}
+    });
+    this.videoElement.srcObject = stream;
+    this.videoElement.setAttribute('playsinline', true);
+    this.videoElement.play(); //trigger the play operations
+  }
+
+  //helper functions
+  stopScan() {
+    this.scanActive = false;
+  }
+
+  reset() {
+    this.scanResult = null;
+  }
+
+  async showQRToast () {
+    const toast = await this.toastCtrl.create({
+      message: 'Open ${this.scanResult}?',
+      position: 'top',
+      buttons: [
+        {
+          text:'Open',
+          handler:() => {
+            window.open(this.scanResult, '_system', 'location=yes');
+          }
+        }
+      ]
+    });
+    toast.present();
+  }
 }
